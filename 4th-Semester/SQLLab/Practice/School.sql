b49201de68f04ffcfa438b73702430ce72b22833
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2025-02-06 12:43:44 +0530

    SQL Lab

---
+++ 4th-Semester/SQLLab/Practice/School.sql
@@ -0,0 +1,15 @@
+create database school;
+
+use school;
+
+CREATE TABLE Students (
+	StudentID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName CHAR(20),
+	Age INT,
+    DateOfBirth DATE,
+	Marks FLOAT,
+	Email VARCHAR(100)
+);
+
+desc Students;
